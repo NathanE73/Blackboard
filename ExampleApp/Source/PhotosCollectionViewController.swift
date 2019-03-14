ade9c52362f4fc925b615f152c0d24934e65115b
Nate <knate@knate.com>
2019-03-14 09:31:08 -0400

    Add example photo storyboard

---
+++ ExampleApp/Source/PhotosCollectionViewController.swift
@@ -0,0 +1,53 @@
+//
+// Copyright (c) 2019 Nathan E. Walczak
+//
+// MIT License
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+import UIKit
+
+class PhotosCollectionViewController: UICollectionViewController {
+    
+    let photoViewModels = PhotoViewModel.examples
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        handleSegue(segue, sender: sender)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photoViewModels.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let photoViewModel = photoViewModels[indexPath.item]
+        return dequeuePhotoCell(from: collectionView, for: indexPath) { cell in
+            cell.imageView.image = photoViewModel.image
+        }
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let photoViewModel = photoViewModels[indexPath.item]
+        performShowPhotoSegue { segue in
+            segue.photoViewModel = photoViewModel
+        }
+    }
+    
+}
